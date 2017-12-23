fef88d11c54d719bab72490b11384c6ba2252aa8
Anton Dranitsyn <anton-drancyn@mail.ru>
2017-12-23 14:06:04 +0300

    Добавил динамическое отображение списков с нормальным именованием.

--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.defaulttags import register
 
 from .models import Projects, TodoList
 
@@ -7,6 +8,15 @@ from .models import Projects, TodoList
 def index(request):
     return render(request, "__base.html", {
         "project_info": Projects.objects.filter(title='Лунный ровер'),
-        "todo": list(TodoList.objects.filter(category=i) for i in range(len(TodoList.CATEGORY)))
-    }
+        "todo": list(TodoList.objects.filter(category=i) for i in range(len(TodoList.CATEGORY))),
+        "category": dict(TodoList.CATEGORY[i] for i in range(len(TodoList.CATEGORY))),
+        "status": dict(TodoList.STATUS[i] for i in range(len(TodoList.STATUS))),
+        "importance": dict(TodoList.IMPORTANCE[i] for i in range(len(TodoList.IMPORTANCE))),
+
+    },
                   )
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
